e0b37fb8d992fc609b8c2a293d1c6bf75f9d74d8
EAGLE EYES <82096553+EagleEyesPrim@users.noreply.github.com>
2024-05-24 07:31:05 +0300

    Update 1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'eagle_scanner=eagle_packets_scanner:main',
+            'eagle_scanner=/usr/local/bin/eagle_scanner:main',
         ],
     },
 )
