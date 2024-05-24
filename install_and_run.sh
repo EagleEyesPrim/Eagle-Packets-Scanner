bba632646ab5b6a8cb83f22ac592b7eb771dadb6
EAGLE EYES <82096553+EagleEyesPrim@users.noreply.github.com>
2024-05-24 06:55:47 +0300

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -8,6 +8,12 @@ pip install -r requirements.txt
 echo "Installing the package..."
 python setup.py install
 
+# Move the program file to an easily accessible folder
+sudo cp /path/to/eagle_scanner /usr/local/bin
+
+# Create a symbolic link to the playable file in a folder that is included in PATH
+sudo ln -s /usr/local/bin/eagle_scanner /usr/bin/eagle_scanner
+
 # Run the program
 echo "Running the Eagle Packets Scanner..."
 sudo eagle_scanner
