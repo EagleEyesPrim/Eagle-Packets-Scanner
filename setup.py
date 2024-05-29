575ad8d52f1f5586662352f4ac1515b55e774120
EAGLE EYES <82096553+EagleEyesPrim@users.noreply.github.com>
2024-05-29 21:52:46 +0300

    1.8.5

--- setup.py
+++ setup.py
@@ -5,17 +5,41 @@ setup(
     version='1.8.5',
     packages=find_packages(),
     install_requires=[
-        'blessed==1.18.1',
-        'ipwhois==1.3.0',
-        'mac-vendor-lookup==0.2.3',
-        'psutil==5.8.0',
-        'scapy==2.4.5',
-        'termcolor==1.1.0',
-        'tabulate==0.8.9'
+        'blessed',
+        'ipwhois',
+        'mac-vendor-lookup',
+        'psutil',
+        'scapy',
+        'termcolor',
+        'tabulate',
+        'build'
     ],
     entry_points={
         'console_scripts': [
-            'epscanner = eagle_packets_scanner:main'
+            'epscanner = eagle_packets_scanner:main'  # تأكد أن `main` هو دالة البدء في `eagle_packets_scanner`
         ]
     },
+    author='Eagle Eyes Prim',
+    description='Eagle Packets Scanner is a network monitoring and analysis tool.',
+    long_description="""Eagle Packets Scanner is a powerful network tool for monitoring and analyzing network traffic in real-time. 
+    It provides users with the ability to capture, inspect, and analyze packets flowing through their network interfaces. 
+    With its intuitive interface and rich features, Eagle Packets Scanner is suitable for network administrators, cybersecurity professionals, and enthusiasts 
+    who want to gain insights into their network activity.""",
+    long_description_content_type='text/plain',
+    url='https://github.com/EagleEyesPrim/eagle-packets-scanner',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities'
+    ],
+    python_requires='>=3.8'
 )
